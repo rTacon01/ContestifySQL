43017a582b162526626644648db96a22ea849b4c
rTacon01 <romain.tacon01@gmail.com>
2024-09-28 16:03:34 +0200

    Update SQL+Jeux de données.sql
    delete bdd

--- SQL+Jeux de données.sql
+++ SQL+Jeux de données.sql
@@ -17,10 +17,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données : `zdl3-ztaconro0_1`
---
-
 DELIMITER $$
 --
 -- Procédures
